5ecd1d47470ad8cb4035d00c9b4a396a41cbc7df
Srinath Shretha <srinathshrestha9890@gmail.com>
2021-01-23 20:46:24 +0530

    task given on date: 23/01/2021

---
+++ list.py
@@ -0,0 +1,8 @@
+# -------------------------------------------------question----------------------------------------
+# question to input 10 nums in a list and then display all the elements by multiplying 2
+print('Enter the elements of these list : ')
+lis =[]
+for i in range(0,5):
+    a = int(input())
+    lis.append(a*2)
+print(lis)
