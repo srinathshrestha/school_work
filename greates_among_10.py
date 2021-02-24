5f913342f1d08c6d2db4b12ce0a6b87d756271e8
Srinath Shretha <srinathshrestha9890@gmail.com>
2021-02-24 17:44:47 +0530

    date; 24-02-2021

--- greates_among_10.py
+++ greates_among_10.py
@@ -1,18 +1,30 @@
-n = []
-k = len(n)
+# n = []
+# k = len(n)
+# for i in range(10):
+#     ellement = int(input(f"Enter the {i+1}.Numbers : "))
+#     n.append(ellement)
+# def bubble(n):
+#     if len(n)==10:
+#         for i in range(len(n) - 1, 0, -1):
+#             for j in range(i):
+#                 if n[j] > n[j + 1]:
+#                     t = n[j]
+#                     n[j] = n[j + 1]
+#                     n[j + 1] = t
+#
+# print('the greatest number in that you have entered is',n[k-1])
+#
+#
+#
+#
+
+l=[]
+g=0
 for i in range(10):
-    ellement = int(input(f"Enter the {i+1}.Numbers : "))
-    n.append(ellement)
-def bubble(n):
-    if len(n)==10:
-        for i in range(len(n) - 1, 0, -1):
-            for j in range(i):
-                if n[j] > n[j + 1]:
-                    t = n[j]
-                    n[j] = n[j + 1]
-                    n[j + 1] = t
+    a = int(input("enter the elements : "))
+    l.append(a)
 
-print('the greatest number in that you have entered is',n[k-1])
-
-
-
+for i in range(10):
+    if l[i]>g:
+        l[i]=g
+        print(f"{g} is the greates number.")
---
+++ joke.py
@@ -0,0 +1,3 @@
+print("In USA when people urinets in public they call them sex ofender")
+input("Press enter for the punchline")
+print("but in india we say,'chee papa'!! ")
